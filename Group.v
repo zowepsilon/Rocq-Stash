cd11ed8f5afac94784d9cfdbcb07a4ad2f653ced
Sarah <74245537+Sarenard@users.noreply.github.com>
2025-09-16 13:19:23 +0200

    Start of group file

---
+++ Group.v
@@ -0,0 +1,34 @@
+Record Group := {
+    (*Data*)
+    Grp :> Type;
+    Loi : Grp -> Grp -> Grp;
+    Id: Grp;
+
+    (*Contraints*)
+    associative := forall x y z : Grp, Loi (Loi x y) z = Loi x (Loi y z);
+    id_left : forall x : Grp, Loi Id x = x;
+    id_right : forall x : Grp, Loi x Id = x;
+    inverse_left : forall x : Grp, exists x' : Grp, Loi x' x = Id;
+    inverse_right : forall x : Grp, exists x' : Grp, Loi x x' = Id;
+}.
+
+Definition is_neutral {G: Group} (e: G) :=
+    (forall y : G, (Loi G) e y = y) /\ (forall y : G, (Loi G) y e = y).
+
+Theorem uniq_initial:
+  forall (G: Group) (x y: Grp G), is_neutral x -> is_neutral y -> x = y.
+Proof.
+  (*Woah thats a bad proof*)
+  intros.
+  assert (x = (Loi G) x y).
+  unfold is_neutral in H0.
+  destruct H0 as [H1 H2].
+  rewrite H2. reflexivity.
+  assert ((Loi G ) x y = y).
+  unfold is_neutral in H.
+  destruct H as [H3 H4].
+  rewrite H3. reflexivity.
+  rewrite H1. rewrite H2. trivial.
+Qed.
+
+
